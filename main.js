142286cb2d9373382afd5c37f7c4b7783565c6f6
Student <carla.granof@gmail.com>
2015-01-21 15:41:29 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,134 @@
+// 1. tripleFive
+/*
+var tripleFive = function(){
+	for(var i=1; i < 4; i++) {
+		console.log("Five!");
+	}
+	
+} 
+
+// 2. lastLetter
+
+var lastLetter = function(str) {
+	
+	console.log(str.charAt(str.length - 1));
+}
+lastLetter("walrus");
+
+
+// 3. square
+
+var square = function(x) {
+	console.log(x * x);
+}
+
+square(5)
+
+
+// 4. negate
+
+var negate = function(x) {
+	console.log(-x);
+}
+negate(3)
+
+
+
+// 5. toArray
+
+var toArray = function(a, b, c) {
+	var createArray = [];
+	console.log(createArray.push(a, b, c));
+}
+
+toArray(1,2,3);
+
+
+
+// 6. startsWithA
+
+var startsWithA = function(item) {
+	if(item.charAt(0) === "a") {
+		console.log(item = true);
+	}
+	else{
+		console.log(item = false);
+	}
+}
+
+startsWithA("bee");
+
+
+
+
+// 7. excite
+
+var excite = function(item) {
+	console.log(item + "!!!");
+}
+
+excite("yes");
+
+
+
+
+// 8. sun
+
+var sun = function(str) {
+	console.log(str.indexOf("sun"))
+}
+
+sun("asunder");
+
+
+
+
+
+// 9. tiny
+
+var tiny = function(x) {
+	if(x > 0 && x < 1) {
+		console.log(x = true);
+	}
+	else {
+		console.log(x = false);
+	}
+}
+
+tiny(.5)
+
+// 10. getSeconds
+
+var getSeconds = function(time) {
+	var s = time.split(':')
+	console.log(s[0]* 60 + s[1]*1);
+}
+
+
+getSeconds("1:30");
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
